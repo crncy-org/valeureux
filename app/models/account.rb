71cfd62ddc708067dac7010b963b18c3d6d68e5c
frederic lambrechts <f.lambrechts@gmail.com>
2015-03-03 12:46:30 +0100

    account number generation method added

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,32 @@
 class Account < ActiveRecord::Base
+  before_save :generate_unique_account_number
 
   def amount
     return 2000
   end
 
+  def generate_unique_account_number
+    generate_new = nil
+    loop do
+      generate_new = generate_account_number
+      break unless Account.exists?(number: generate_new)
+    end
+
+    self.number = generate_new
+  end
+
+  def generate_account_number
+    random_figure = rand(1000000000..9999999999)
+    division = random_figure / 97
+    multiplication = division * 97
+    rest = random_figure - multiplication
+
+    if rest < 10
+      number = random_figure.to_s + rest.to_s
+    else
+      number = "#{random_figure}0#{rest}"
+    end
+    number
+  end
 end
 
