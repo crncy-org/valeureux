c4c02f6e7af133fbecdcaa82c9a1b18fa21c84b0
frederic lambrechts <f.lambrechts@gmail.com>
2015-03-04 16:24:27 +0100

    initialize activity page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   # Zelf toegevoegd
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @mainTitle = "Welcome in Inspinia Rails Seed Project"
-    @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
+    @balance = current_user.account.balance
+    # @transaction_history = Account.current_user_account.transactions
   end
 
   def minor
+    @wire_reference = current_user.account.number
   end
 end
