fd49e1247d244eff3533f2dbaffcd0990299eb2a
frederic lambrechts <f.lambrechts@gmail.com>
2015-03-13 14:52:05 +0100

    landing page fix, order transaction history fix

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,10 @@ class Transaction < ActiveRecord::Base
   belongs_to :sender, :class_name => 'Account'
   belongs_to :receiver, :class_name => 'Account'
 
+
   TRANSACTION_TYPE = ["transfer","payment"]
 
+  default_scope { order('created_at DESC') }
   # after_create do |transaction|
   #   client = Twilio::REST::Client.new
   #   client.messages.create(
